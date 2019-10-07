7714826f526cb3f4191e1cf50a7e95e5f9aeeb30
Mahaut <mahautdeguilhem@gmail.com>
2019-10-07 18:04:15 +0200

    terminé!

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,17 +1,18 @@
 def time_string (time)
+
 	if time < 60 
-		return "00:00:#{time}"
+		return "00:00:" + "%02d"%time    # le "%02d"% permet de mettre un "leading zero" si il n'y a qu'une unité; "%03d"% signifierait un format 000
 	end
 		if 60 <= time && time < 3599
 		minutes = time/60 
 		seconds = time%60 
-		return "00:#{minutes.to_i}:#{seconds}" 
+		return "00:" + "%02d"% minutes.to_s + ":" + "%02d"%seconds.to_s 
 	end 
 
 	if time >= 3600
 		hours = time/3600  
 		minutes =time%3600/60 
 		seconds = time%3600%60
-		return "#{hours.to_s}:#{minutes.to_s}:#{seconds.to_s}"
+		return "%02d"%hours.to_s+":"+"%02d"%minutes.to_s+":"+"%02d"%seconds.to_s
 	end
 end
--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,8 +1,12 @@
 def translate (phrase)
-	phrase = phrase.split
-	if phrase.map {|word| word.start_with? /[aeoiu]/} == false
-	phrase.reverse!
-	end
-	phrase.map{|word| word + "ay"}.join (" ")
+    vowels = %w{a e i o u}
+    phrase.gsub(/(\A|\s)\w+/) do |str|
+            str.strip!
+        while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+            str += str[0]
+            str = str[1..-1]
+        end
+        str  = ' ' + str + 'ay'
+    end.strip
 
 end
